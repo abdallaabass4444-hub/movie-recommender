7d8c0be85269b2331d249150ff5cbc4df329c7d3
Abdalla Abass <abdallaabass4444-hub>
2026-05-15 01:17:12 +0300

    Fix indentation error

--- project1_data.py
+++ project1_data.py
@@ -12,7 +12,7 @@ def load_movielens_100k(data_path=DATA_PATH):
 
     print('Loading dataset from CSV files...')
     movies = pd.read_csv("data/movies.csv")
-ratings = pd.read_csv("data/ratings.csv")
+    ratings = pd.read_csv("data/ratings.csv")
 
     return ratings, movies
 
